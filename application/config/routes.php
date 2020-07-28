a1783ea946fb3f15339318e6322673fde93b097e
iyeong-gyo <whdnseowkd@naver.com>
2020-07-28 20:50:10 +0900

    config/routes.php 수정

--- application/config/routes.php
+++ application/config/routes.php
@@ -55,8 +55,17 @@ $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
 */
+
+
+$route['default_controller'] = 'news/index';
+
+//$route['(:any)'] = 'pages/view/$1';
+
+/* news */
+$route['news'] = 'news/index';
 $route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'news/view/$1';
-$route['news'] = 'news';
-$route['(:any)'] = 'pages/view/$1';
-$route['default_controller'] = 'pages/view';
+
+
+
+
